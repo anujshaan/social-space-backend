a87bc805c4745880494c6bde92e3abc2920ccd1c
Anuj Shaan <anujprakashshaan@gmail.com>
2023-01-23 15:20:35 +0530

    fixed bug

--- server.js
+++ server.js
@@ -49,12 +49,12 @@ mongoose.connect(URL,{
     console.log("Database connected successfully");
 })
 
-if(process.env.NODE_ENV ==='production'){
-    app.use(express.static('client/build'))
-    app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+// if(process.env.NODE_ENV ==='production'){
+//     app.use(express.static('client/build'))
+//     app.get('*',(req,res)=>{
+//         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+//     })
+// }
 
 
 const port = process.env.PORT || 5000;
